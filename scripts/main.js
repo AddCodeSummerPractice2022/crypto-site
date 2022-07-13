ba9af38413c6e65cb275f3adbd534577042e5372
bublogam <108838939+mikhailGaluzin@users.noreply.github.com>
2022-07-13 14:26:56 +0300

    Update main.js
    Добавил функцию конвертирования

--- scripts/main.js
+++ scripts/main.js
@@ -34,4 +34,39 @@ fetch('https://api.coincap.io/v2/assets?ids=bitcoin,ethereum,dogecoin,tron,cosmo
         document.getElementById("REN2").innerHTML = parseInt(commits.data[8].changePercent24Hr * 1000) / 1000 + " %" 
     });
   }
-  setInterval(getRequest, 500)
+setInterval(getRequest, 500)
+
+window.onload=function(){
+
+  const from_currencyEl = document.getElementById('from_curency');
+  const from_ammountEl = document.getElementById('input');
+  const to_currencyEl = document.getElementById('to_curency');
+  const to_ammountEl = document.getElementById('output');
+
+
+  from_currencyEl.addEventListener('change', calculate);
+  from_ammountEl.addEventListener('input', calculate);
+  to_currencyEl.addEventListener('change', calculate);
+  to_ammountEl.addEventListener('input', calculate);
+
+
+  function calculate() {
+  
+    const from_currency = from_currencyEl.value;
+    const to_currency = to_currencyEl.value;
+    
+    fetch(`https://api.coincap.io/v2/assets?ids=${from_currency},${to_currency}`)
+    .then(res => res.json())
+    .then(res => {
+    let rate = res.data[0].priceUsd;
+    let rate1 = res.data[1].priceUsd;
+    to_ammountEl.value = ((from_ammountEl.value * rate)/rate1).toFixed(4);
+    if(from_currencyEl == to_currencyEl){
+      to_ammountEl.value = from_ammountEl.value;
+    }
+    
+    })
+  }
+  
+  calculate();
+}
