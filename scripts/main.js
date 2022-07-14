1b27dcece5923042d539a619e08ced09e8285ce4
bublogam <108838939+mikhailGaluzin@users.noreply.github.com>
2022-07-14 16:45:29 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,55 @@
-let lastPrice  = null;
-const Ws = new WebSocket('wss://ws.coincap.io/prices?assets=bitcoin')
-let stockpriceEl = document.getElementById('BTC');
+const { createApp } = Vue;
 
-Ws.onmessage = (event) => {
-    let stcobj = JSON.parse(event.data);
-    let price  = parseFloat(stcobj.bitcoin).toFixed(4);
-    
-    if(!lastPrice || lastPrice === price){
-      document.getElementById('BTC').innerHTML = '<span style="color:black;">' +
-      price +  '</span>'; 
-    }else if(price > lastPrice){
-      document.getElementById('BTC').innerHTML = '<span style="color:green;">' +
-      price +  '</span>';
-    }if(price < lastPrice){
-      document.getElementById('BTC').innerHTML = '<span style="color:red;">' +
-      price  + '</span>';
+const quotes = ["bitcoin","ethereum","dogecoin","tron","cosmos","stellar","litecoin","zcash","ren"];
+
+const prepareHttpRequest = (url) =>
+  fetch(url).then((response) => response.json());
+
+const mapAssetFromDto = (assetDto) =>( {
+  assetId: assetDto.symbol,
+  name: assetDto.id,
+  priceUSD: parseInt(assetDto.priceUsd * 1000) / 1000,
+  volume: parseInt(assetDto.volumeUsd24Hr * 1000) /1000,
+  
+});
+
+const getAssets = () =>
+  prepareHttpRequest(
+    `https://api.coincap.io/v2/assets?ids=${quotes.join(
+      ","
+    )}`)
+    .then((data) =>
+    data.data.map((assetDto) => mapAssetFromDto(assetDto))
+  );
+
+// vue app
+
+createApp({
+  data() {
+    return {
+      loading: false,
+      error: null,
+      assets: null,
+      
+    };
+  },
+  computed: {
+    isNonEmptyError() {
+      return this.error !== null;
+    },
+    isNonEmptyAssets() {
+      return this.assets !== null;
+    },
+  },
+  async mounted() {
+    try {
+      this.loading = true;
+      this.assets = await getAssets();
+    } catch (err) {
+      console.error(err);
+      this.error = err;
+    } finally {
+      this.loading = false;
     }
-    
-    lastPrice = price;
-    
-}
+  },
+}).mount("#app");
